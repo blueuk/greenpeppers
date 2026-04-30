19e359265925d05e338a25339d48e01a2a64ff01
blueuk <31227382+blueuk@users.noreply.github.com>
2026-04-30 12:48:13 +0900

    receiptsInfo.js 만들기

---
+++ js/receiptsInfo.js
@@ -0,0 +1,19 @@
+function copyAccount(accNum) {
+        // 하이픈 제거 로직
+        const plainNumber = accNum.replace(/-/g, '');
+        
+        // 클립보드 복사
+        navigator.clipboard.writeText(plainNumber).then(() => {
+            showToast();
+        }).catch(err => {
+            console.error('복사 실패:', err);
+        });
+}
+
+function showToast() {
+    const toast = document.getElementById('toast');
+    toast.style.display = 'block';
+    setTimeout(() => {
+        toast.style.display = 'none';
+    }, 2000);
+}
